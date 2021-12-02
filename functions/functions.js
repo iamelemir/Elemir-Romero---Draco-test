f9e2ade9584068061d8a6716cdd8bbc4da446368
iamelemir <elemir18@gmail.com>
2021-12-01 22:38:41 -0500

    inizializate styles

--- functions/functions.js
+++ functions/functions.js
@@ -1,3 +1,19 @@
-function name(params) {
 
+
+const start = document.getElementById("start").style.display = "block";
+
+// const squareYellow = document.getElementById("squareYellow").style.display = "block";
+// const triangleBlue = document.getElementById("triangleBlue").style.display = "none";
+// const circleRed = document.getElementById("circleRed").style.display = "none";
+// const textYellow = document.getElementById("textYellow").style.display = "none";
+// const textBlue = document.getElementById("textBlue").style.display = "none";
+// const textRed = document.getElementById("textRed").style.display = "none";
+
+function displayShow() {
+    console.log('estoy aqui')
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
