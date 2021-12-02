c06ffb9f0c9f63b1c26c2cf152bddfebe4fa181b
iamelemir <elemir18@gmail.com>
2021-12-02 12:18:20 -0500

    changes

--- docs/functions.js
+++ docs/functions.js
@@ -1,49 +1,49 @@
 
 $(document).ready(function () {
     $('#boton-back').click(function (event) {
-        var url = window.location.href;
+        var url = window.location.href
 
         var ficheroActual = url.substring(url.lastIndexOf('/') + 1)
 
         switch (ficheroActual) {
             case 'yellow.html':
                 urlFinal = url.replace(url.substring(url.lastIndexOf('/') + 1), 'index.html')
-                window.location.replace(urlFinal);
-                break;
+                window.location.replace(urlFinal)
+                break
             case 'blue.html':
                 urlFinal = url.replace(url.substring(url.lastIndexOf('/') + 1), 'yellow.html')
                 window.location.replace(urlFinal);
-                break;
+                break
             case 'red.html':
                 urlFinal = url.replace(url.substring(url.lastIndexOf('/') + 1), 'blue.html')
                 window.location.replace(urlFinal);
-                break;
+                break
         }
-    });
+    })
 
     $('#boton-next').click(function (event) {
-        var url = window.location.href;
+        var url = window.location.href
 
         var ficheroActual = url.substring(url.lastIndexOf('/') + 1)
 
         switch (ficheroActual) {
             case 'index.html':
                 urlFinal = url.replace(url.substring(url.lastIndexOf('/') + 1), 'yellow.html')
-                window.location.replace(urlFinal);
+                window.location.replace(urlFinal)
                 break;
             case 'yellow.html':
                 urlFinal = url.replace(url.substring(url.lastIndexOf('/') + 1), 'blue.html')
-                window.location.replace(urlFinal);
+                window.location.replace(urlFinal)
                 break;
             case 'blue.html':
                 urlFinal = url.replace(url.substring(url.lastIndexOf('/') + 1), 'red.html')
-                window.location.replace(urlFinal);
+                window.location.replace(urlFinal)
                 break;
         }
     });
-});
+})
 
 function show() {
-    var botonBack = document.getElementById("boton-back");
-    botonBack.disabled = true;
+    var botonBack = document.getElementById("boton-back")
+    botonBack.disabled = true
 }
